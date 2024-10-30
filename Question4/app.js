24b2d93e9ee8349ade7ad38aa2873a9fddd6c0e7
devendra <devendra5141697@gmail.com>
2024-10-30 09:47:06 +0530

    Question for done

---
+++ Question4/app.js
@@ -0,0 +1,18 @@
+let form = document.getElementById('form')
+let result = document.getElementById('result');
+
+
+function fun(obj){
+    let l = Object.entries(obj).length;  // calculate lenght of the string
+    return l;
+}
+
+// Take input from the user using form
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let str = form.elements[0].value;
+    let object = JSON.parse(str) // convert in object
+    let res = fun(object);
+    result.innerHTML = `<h2> Lenght of Object is : ${res} </h2>`         // show on the web page
+})
+
