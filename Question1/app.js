37c782ed3b2d39387d9bb09b425103ef73e43ef0
devendra <devendra5141697@gmail.com>
2024-10-30 15:22:55 +0530

    basic styling added to each question

--- Question1/app.js
+++ Question1/app.js
@@ -1,5 +1,7 @@
 const dt1 = document.getElementById('date1');
 const dt2 = document.getElementById('date2');
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2')
 
 const date = new Date();
 const d = date.toLocaleDateString();
@@ -9,5 +11,11 @@ let dd = dArray[0];
 let mm = dArray[1];
 let yyyy = dArray[2];
 
-dt1.innerHTML = `<h1>DD/MM/YYYY formate:   ${dd}/${mm}/${yyyy} <h1>`
-dt2.innerHTML = `<h1>MM/DD/YYYY formate:   ${mm}/${dd}/${yyyy} <h1>`
+btn1.addEventListener('click', (e) => {
+   dt1.innerHTML = `<h1> ${dd}/${mm}/${yyyy} <h1>`
+});
+
+btn2.addEventListener('click', (e) => {
+    dt2.innerHTML = `<h1> ${mm}/${dd}/${yyyy} <h1>`
+ });
+
