2bd625671f47a9e2cbd8c34ef51212ae344edca0
devendra <devendra5141697@gmail.com>
2024-10-30 10:10:23 +0530

    question 5 done

---
+++ Question5/app.js
@@ -0,0 +1,17 @@
+const result = document.getElementById('result');
+
+
+let r = Math.floor(Math.random()*10+1);   // generate random no. from 1 to 10 
+
+const userInput = parseInt(prompt("Enter the number between 1 to 10"));
+if(userInput < 1 || userInput > 10){
+    alert("Please enter a number between 1 to 10");
+} else if(userInput == NaN) {
+    alert("Please enter a number between 1 to 10");
+}   
+// above case will check for the wrong input 
+else if(r === parseInt(userInput)){
+    result.innerHTML = "Good Work"
+} else {
+    result.innerHTML = "Not matched"
+}
